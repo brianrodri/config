31a8bc9d47019b3adb5540d1a833935e5ffc345b
Brian Rodriguez <brian@brianrodri.com>
2025-01-03 16:25:35 -0500

    feat: show background jobs

--- p10k.zsh
+++ p10k.zsh
@@ -44,7 +44,7 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # status                  # exit code of the last command
     # command_execution_time  # duration of the last command
-    # background_jobs         # presence of background jobs
+    background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     # asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
