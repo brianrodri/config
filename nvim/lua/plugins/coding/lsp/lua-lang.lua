132a636617a9e3cf39f5c8163dab14bca84e0273
Brian Rodriguez <brian@brianrodri.com>
2025-04-28 12:00:32 -0400

    fix: safely extend nvim-treesitter languages to auto-install

--- nvim/lua/plugins/coding/lsp/lua-lang.lua
+++ nvim/lua/plugins/coding/lsp/lua-lang.lua
@@ -5,7 +5,10 @@ return {
         opts = { library = { path = "${3rd}/luv/library", words = { "vim%.uv" } } },
         optional = true,
     },
-    { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = { "lua", "luadoc" } } },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts) opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, { "lua", "luadoc" }) end,
+    },
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         opts = function(_, opts)
