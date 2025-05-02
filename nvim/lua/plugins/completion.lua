d7f5c70c2899e10d7acf4fb50739f7e41681016a
Brian Rodriguez <brian@brianrodri.com>
2025-05-02 04:09:25 -0400

    feat!: remove unused LuaSnip plugin

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -3,7 +3,6 @@ return {
     event = "VimEnter",
     version = "1.*",
     dependencies = {
-        "L3MON4D3/LuaSnip",
         "folke/lazydev.nvim",
         "MahanRahmati/blink-nerdfont.nvim",
     },
@@ -12,12 +11,10 @@ return {
         completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
         fuzzy = { implementation = "prefer_rust_with_warning" },
         keymap = { preset = "default" },
-        snippets = { preset = "luasnip" },
         sources = {
             default = {
                 "lsp",
                 "path",
-                "snippets",
                 "buffer",
                 "lazydev",
                 "nerdfont",
