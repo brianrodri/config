90e93317596eeac8b307c0bba74a3d17a2725243
Brian Rodriguez <brian@brianrodri.com>
2025-05-03 18:23:57 -0400

    feat!: use default treesitter opts

--- nvim/lua/plugins/text-support/navigation.lua
+++ nvim/lua/plugins/text-support/navigation.lua
@@ -22,11 +22,6 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         main = "nvim-treesitter.configs",
-        opts = {
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        },
     },
 
     { -- Treesitter + textobjects
