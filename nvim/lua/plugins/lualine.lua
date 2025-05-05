955ed1d6afa891edd7cad1914bfdca2412aa3485
Brian Rodriguez <brian@brianrodri.com>
2025-05-05 17:53:26 -0400

    perf: use gitsigns for diff information

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,25 @@
+local function get_gitsigns_diff()
+  local gitsigns = vim.b.gitsigns_status_dict
+  return gitsigns and { added = gitsigns.added, modified = gitsigns.changed, removed = gitsigns.removed }
+end
+
 ---@module "lazy"
 ---@type LazySpec
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = { theme = "everforest" },
+  opts = function()
+    return {
+      theme = "everforest",
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "b:gitsigns_head", { "diff", source = get_gitsigns_diff }, "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "lsp_status" },
+        lualine_z = { "location" },
+      },
+      extensions = { "neo-tree", "nvim-dap-ui", "trouble" },
+    }
+  end,
 }
