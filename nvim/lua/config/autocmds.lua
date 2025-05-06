295a88bfbefb33ead9772bb6914a0b09438acd9d
Brian Rodriguez <brian@brianrodri.com>
2025-05-06 18:05:07 -0400

    refactor: move keymaps into constant

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,60 @@
+---@module "which-key"
+---@type wk.Spec[]
+---Keymaps to attach to _every_ LSP.
+local LSP_WHICH_KEY_SPECS = {
+  {
+    "<leader>cr",
+    function() vim.lsp.buf.rename() end,
+    desc = "Rename Symbol",
+  },
+  {
+    "<leader>ca",
+    function() vim.lsp.buf.code_action() end,
+    desc = "Code Action",
+    mode = { "n", "x" },
+  },
+  {
+    "<leader>c*",
+    function() require("trouble").toggle("lsp_references") end,
+    desc = "References",
+  },
+  {
+    "<leader>cd",
+    function() require("trouble").toggle("lsp_declarations") end,
+    desc = "Declaration",
+  },
+  {
+    "<leader>cD",
+    function() require("trouble").toggle("lsp_definitions") end,
+    desc = "Definitions",
+  },
+  {
+    "<leader>cy",
+    function() require("trouble").toggle("lsp_implementations") end,
+    desc = "Implementations",
+  },
+  {
+    "<leader>cY",
+    function() require("trouble").toggle("lsp_type_definitions") end,
+    desc = "Type Definitions",
+  },
+  {
+    "<leader>cs",
+    function() require("trouble").toggle("lsp_document_symbols") end,
+    desc = "Document Symbols",
+  },
+  {
+    "<leader>cj",
+    function() require("trouble").toggle("lsp_incoming_calls") end,
+    desc = "Incoming Calls",
+  },
+  {
+    "<leader>ck",
+    function() require("trouble").toggle("lsp_outgoing_calls") end,
+    desc = "Outgoing Calls",
+  },
+}
+
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
@@ -9,69 +66,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp-keymaps", { clear = true }),
   callback = function(args)
-    require("which-key").add({
-      {
-        "<leader>cr",
-        function() vim.lsp.buf.rename() end,
-        desc = "Rename Symbol",
-        buffer = args.buf,
-      },
-      {
-        "<leader>ca",
-        function() vim.lsp.buf.code_action() end,
-        desc = "Code Action",
-        mode = { "n", "x" },
-        buffer = args.buf,
-      },
-      {
-        "<leader>c*",
-        function() require("trouble").toggle("lsp_references") end,
-        desc = "References",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cd",
-        function() require("trouble").toggle("lsp_declarations") end,
-        desc = "Declaration",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cD",
-        function() require("trouble").toggle("lsp_definitions") end,
-        desc = "Definitions",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cy",
-        function() require("trouble").toggle("lsp_implementations") end,
-        desc = "Implementations",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cY",
-        function() require("trouble").toggle("lsp_type_definitions") end,
-        desc = "Type Definitions",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cs",
-        function() require("trouble").toggle("lsp_document_symbols") end,
-        desc = "Document Symbols",
-        buffer = args.buf,
-      },
-      {
-        "<leader>cj",
-        function() require("trouble").toggle("lsp_incoming_calls") end,
-        desc = "Incoming Calls",
-        buffer = args.buf,
-      },
-      {
-        "<leader>ck",
-        function() require("trouble").toggle("lsp_outgoing_calls") end,
-        desc = "Outgoing Calls",
-        buffer = args.buf,
-      },
-    })
+    local specs = vim
+      .iter(LSP_WHICH_KEY_SPECS)
+      :map(function(spec) return vim.tbl_extend("force", { buffer = args.buf }, spec) end)
+      :totable()
+    require("which-key").add(specs)
   end,
 })
 
