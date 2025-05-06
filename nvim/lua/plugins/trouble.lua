6168bd1a01f0a1ac361fa78c1a90c7cb8c7a38e0
Brian Rodriguez <brian@brianrodri.com>
2025-05-06 19:08:47 -0400

    feat: filter tags shown by todo shortcut

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,3 +1,9 @@
+local TODO_TAGS = { "fix", "todo", "hack", "perf", "warn" }
+
+local todo_filter = function(items)
+  return vim.tbl_filter(function(i) return vim.tbl_contains(TODO_TAGS, string.lower(i.tag)) end, items)
+end
+
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -5,8 +11,13 @@ return {
   cmd = "Trouble",
   keys = {
     { "<leader>xx", function() require("trouble").toggle("diagnostics") end, desc = "Diagnostics" },
-    { "<leader>xt", function() require("trouble").toggle("todo") end, desc = "Todo" },
-    { "<leader>xq", function() require("trouble").toggle("qflist") end, desc = "Quick Fix List" },
+    {
+      "<leader>xt",
+      ---@diagnostic disable-next-line: missing-fields
+      function() require("trouble").toggle({ mode = "todo", filter = todo_filter }) end,
+      desc = "Todo",
+    },
+    { "<leader>xq", function() require("trouble").toggle("qflist") end, desc = "QuickFix List" },
     { "<leader>xl", function() require("trouble").toggle("loclist") end, desc = "Location List" },
   },
 }
