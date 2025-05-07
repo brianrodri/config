3dbbbe94d59e79dadce42036b16d5300c91bb026
Brian Rodriguez <brian@brianrodri.com>
2025-05-07 00:09:15 -0400

    feat: add luacheck for linting

--- nvim/lua/config/lsp/lua-lang.lua
+++ nvim/lua/config/lsp/lua-lang.lua
@@ -1,1 +1,7 @@
+-- LSP
 vim.lsp.enable("lua_ls")
+
+-- Lint
+local ok, lint = pcall(require, "lint")
+if not ok then return vim.notify("config.lsp.markdown-lang called before lint setup", "error") end
+lint.linters_by_ft = vim.tbl_deep_extend("force", lint.linters_by_ft or {}, { markdown = { "luacheck" } })
