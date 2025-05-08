18c533cde93ec84dea4ef459d5da73f95aa4bbf4
Brian Rodriguez <brian@brianrodri.com>
2025-05-08 12:29:17 -0400

    feat: add my Lua utils

--- nvim/lua/my-utils.lua
+++ nvim/lua/my-utils.lua
@@ -1,62 +1,97 @@
-local M = {}
-
-local ISO_DATE_FORMATS = { "%Y", "%Y-%m", "%Y-%m-%d", "%G-W%V" }
-
--- 60s * 60m * 24h * 6d = 518400 seconds
-local SIX_DAYS_IN_SECS = 518400
+local ONE_DAY_IN_SECS = 86400 -- 60s * 60m * 24h
 
 ---@param format string
 ---@param timestring string
 ---@return integer|nil epoch, nil|string err
 ---@nodiscard
-function M.try_parse(format, timestring)
+local function try_parse(format, timestring)
   local ok, result = pcall(vim.fn.strptime, format, timestring)
   if not ok then
     return nil, string.format('vim.fn.strptime("%s", "%s") error: %s', format, timestring, result)
   elseif result == 0 then
-    return nil, string.format('vim.fn.strptime("%s", "%s") error: invalid timestring', format, timestring)
+    return nil, string.format('vim.fn.strptime("%s", "%s") error: string not using format', format, timestring)
   else
     return result, nil
   end
 end
 
-function M.try_parse_iso(iso_str)
-  ---@type string[]
-  local parse_errs = {}
-
-  for _, format in ipairs(ISO_DATE_FORMATS) do
-    local epoch, err = M.try_parse(format, iso_str)
-    if epoch then return epoch, nil end
-    table.insert(parse_errs, err)
-  end
-
-  local debug_info = vim.iter(parse_errs):map(function(err) return "\t" .. err end):join("\n")
-  return nil, string.format('"%s" is not an iso string:%n%s', iso_str, debug_info)
+---@generic ALL_ARGS, LEFT_ARGS
+---@param f fun(...: ALL_ARGS)
+---@param ... LEFT_ARGS
+local function bind_left(f, ...)
+  local bindings = { ... }
+  ---@generic RIGHT_ARGS
+  ---@param ... RIGHT_ARGS
+  return function(...) return f(unpack(bindings), ...) end
 end
 
----@param epoch? number
-function M.iso_week(epoch)
-  return vim.fn.strftime("%G-W%V", epoch)
+---@generic ALL_ARGS, RIGHT_ARGS
+---@param f fun(...: ALL_ARGS)
+---@param ... RIGHT_ARGS
+local function bind_right(f, ...)
+  local bindings = { ... }
+  ---@generic LEFT_ARGS
+  ---@param ... LEFT_ARGS
+  return function(...) return f(..., unpack(bindings)) end
 end
 
----@param epoch? number
-function M.iso_week_number(epoch)
-  return tonumber(vim.fn.strftime("%V", epoch))
+local bind = bind_left
+
+---@param s1 string
+---@param s2 string
+local function concat(s1, s2) return s1 .. s2 end
+
+---@param str string
+---@param prefix? string
+---@param first_prefix? string
+local function indent(str, prefix, first_prefix)
+  prefix = prefix or "\t"
+  first_prefix = first_prefix or prefix
+  local lines = vim.split(str, "\n")
+  local lines_with_prefixes = vim.list_extend(
+    { concat(first_prefix, lines[1]) },
+    vim.iter(vim.list_slice(lines, 2)):map(bind(concat, prefix)):totable()
+  )
+  return vim.iter(lines_with_prefixes):join("\n")
 end
 
----@param epoch? number
-function M.iso_date(epoch)
-  return vim.fn.strftime("%Y-%m-%d", epoch)
+---@param epoch number
+---@return string
+---@nodiscard
+local function iso_week(epoch) return tostring(os.date("%Y-W%V", epoch)) end
+
+---@param epoch number
+---@return number
+---@nodiscard
+local function iso_week_number(epoch) return tonumber(tostring(os.date("%V", epoch)), 10) end
+
+---@param epoch number
+---@return string
+---@nodiscard
+local function iso_date(epoch) return tostring(os.date("%Y-%m-%d", epoch)) end
+
+---@param epoch number
+---@return string
+---@nodiscard
+local function pretty_date(epoch) return tostring(os.date("%a %B %-d %Y", epoch)) end
+
+---@param epoch number
+---@return string
+---@nodiscard
+local function pretty_week(epoch)
+  return vim.iter({ epoch, epoch + 6 * ONE_DAY_IN_SECS }):map(pretty_date):join(" - ")
 end
 
----@param epoch? number
-function M.pretty_date(epoch)
-  return vim.fn.strftime("%a %B %e %Y", epoch)
-end
-
----@param epoch? number
-function M.pretty_week(epoch)
-  return vim.iter({ epoch, epoch + SIX_DAYS_IN_SECS }):map(M.pretty_date):join(" - ")
-end
-
-return M
+return {
+  bind = bind,
+  bind_left = bind_left,
+  bind_right = bind_right,
+  try_parse = try_parse,
+  concat = concat,
+  indent = indent,
+  iso_week = iso_week,
+  iso_week_number = iso_week_number,
+  iso_date = iso_date,
+  pretty_date = pretty_date,
+  pretty_week = pretty_week,
+}
