e844a528c87595396a4ba5c81d5b3a9dc881a17d
Brian Rodriguez <brian@brianrodri.com>
2025-05-08 12:29:27 -0400

    feat: use new template logic

--- nvim/lua/plugins/code-support/obsidian.lua
+++ nvim/lua/plugins/code-support/obsidian.lua
@@ -1,56 +1,67 @@
 local my_utils = require("my-utils")
 
-local INBOX_NOTE_PATH = "0 - Index/Inbox.md"
-
 local ONE_DAY_IN_SECS = 86400
-local ONE_WEEK_IN_SECS = ONE_DAY_IN_SECS * 7
+
+-- TODO: Find a way to include this in template definitions.
+local FOLDER_FORMATS = {
+  ["Daily Template"] = "%Y-%m-%d",
+  ["Weekly Template"] = "%Y-W%V",
+  ["Monthly Template"] = "%Y-%m",
+  ["Yearly Template"] = "%Y",
+}
+
+---@param target obsidian.Path
+---@param template obsidian.Path
+local try_parse = function(target, template)
+  return my_utils.try_parse(FOLDER_FORMATS[template.stem], target.stem)
+end
 
 ---@module "obsidian"
----@type { [string]: fun(note: obsidian.Note): string }
+---@type { [string]: fun(target: obsidian.Path, template: obsidian.Path): string }
 local SUBSTITUTIONS = {
-  pretty_date = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  pretty_date = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.pretty_date(epoch)
   end,
-  pretty_week = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  pretty_week = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.pretty_week(epoch)
   end,
-  curr_iso_date = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  curr_iso_date = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.iso_date(epoch)
   end,
-  prev_iso_date = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  prev_iso_date = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.iso_date(epoch - ONE_DAY_IN_SECS)
   end,
-  next_iso_date = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  next_iso_date = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.iso_date(epoch + ONE_DAY_IN_SECS)
   end,
-  curr_iso_week = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  curr_iso_week = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return my_utils.iso_week(epoch)
   end,
-  curr_iso_week_number = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
+  curr_iso_week_number = function(target, template)
+    local epoch = assert(try_parse(target, template))
     return tostring(my_utils.iso_week_number(epoch))
   end,
-  prev_iso_week = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
-    return my_utils.iso_week(epoch - ONE_WEEK_IN_SECS)
+  prev_iso_week = function(target, template)
+    local epoch = assert(try_parse(target, template))
+    return my_utils.iso_week(epoch - 7 * ONE_DAY_IN_SECS)
   end,
-  next_iso_week = function(note)
-    local epoch = assert(my_utils.try_parse_iso(note.path.stem))
-    return my_utils.iso_week(epoch + ONE_WEEK_IN_SECS)
+  next_iso_week = function(target, template)
+    local epoch = assert(try_parse(target, template))
+    return my_utils.iso_week(epoch + 7 * ONE_DAY_IN_SECS)
   end,
 }
 
-local resolve_note = function()
-  ---@type obsidian.Client
+local resolve_inbox_note = function()
+  local inbox_note_path = "0 - Index/Inbox.md"
   local client = assert(require("obsidian").get_client(), "Obsidian Client not ready")
-  local inbox_note = client:resolve_note(INBOX_NOTE_PATH)
-  assert(inbox_note and inbox_note:exists(), "Obsidian Note not found: " .. INBOX_NOTE_PATH)
+  local inbox_note = client:resolve_note(inbox_note_path)
+  assert(inbox_note and inbox_note:exists(), "Obsidian Note not found: " .. inbox_note_path)
   return client, inbox_note
 end
 
@@ -60,22 +71,12 @@ local open_inbox_note = function()
 end
 
 local append_to_inbox_note = function()
-  require("snacks.input").input({ prompt = "Append To " .. INBOX_NOTE_PATH, default = "- " }, function(value)
-    local update_content = function(lines) return vim.list_extend(lines, { value }) end
+  require("snacks.input").input({ prompt = "Append To Inbox", default = "- " }, function(value)
     local client, inbox_note = resolve_inbox_note()
-    client:write_note(inbox_note, { update_content = update_content })
+    client:write_note(inbox_note, { update_content = my_utils.bind_right(vim.list_extend, { value }) })
     local current_note = client:current_note()
     if current_note and current_note.path == inbox_note.path then client:open_note(current_note) end
   end)
-end
-
-local parse_iso_name = function(iso)
-  if type(iso) ~= "string" then return nil end
-  local iso_date_ok, _, d, m, y = string.find(iso, "^(%d%d%d%d)-(%d%d)-(%d%d)$")
-  if iso_date_ok then return { year = y, month = m, day = d } end
-  local iso_week_ok, _, wky, wkn = string.find(iso, "^(%d%d%d%d)-W(%d%d)$")
-  if iso_date_ok then return {} end
-  return nil
 end
 
 ---@module "lazy"
@@ -95,10 +96,18 @@ return {
         overrides = {
           attachments = { img_folder = "8 - Meta/Attachments" },
           daily_notes = { folder = "1 - Journal/Daily" },
+          notes_subdir = "/",
           disable_frontmatter = true,
-          notes_subdir = "2 - Fleeting Notes",
-          new_notes_location = "notes_subdir",
-          note_id_func = function(title) return title and title:gsub("['\\.]", "") end,
+          note_path_func = function(spec)
+            if my_utils.try_parse("%Y-%m-%d", spec.id) then
+              return string.format("1 - Journal/Daily/%s.md", spec.id)
+            elseif my_utils.try_parse("%Y-W%V", spec.id) then
+              return string.format("1 - Journal/Weekly/%s.md", spec.id)
+            else
+              return string.format("2 - Fleeting Notes/%s.md", spec.id)
+            end
+          end,
+          note_id_func = function(title) return title end,
           templates = {
             folder = "8 - Meta/Neovim Templates",
             substitutions = SUBSTITUTIONS,
@@ -110,10 +119,11 @@ return {
     ui = { enable = false },
   },
   keys = {
-    { "<leader>jf", "<cmd>ObsidianSearch<cr>", desc = "Find Note" },
-    { "<leader>jn", "<cmd>ObsidianNew<CR>",    desc = "New Note" },
-    { "<leader>jt", "<cmd>ObsidianToday<cr>",  desc = "Open Today's Note" },
-    { "<leader>jo", open_note,                 desc = "Open Inbox" },
-    { "<leader>ja", append_to_note,            desc = "Append To Inbox" },
+    { "<leader>j/", "<cmd>ObsidianSearch<cr>", desc = "Search Notes" },
+    { "<leader>jf", "<cmd>ObsidianQuickSwitch<cr>", desc = "Open Note" },
+    { "<leader>jn", "<cmd>ObsidianNew<cr>", desc = "New Note" },
+    { "<leader>jt", "<cmd>ObsidianToday<cr>", desc = "Open Today's Note" },
+    { "<leader>jo", open_inbox_note, desc = "Open Inbox" },
+    { "<leader>ja", append_to_inbox_note, desc = "Append To Inbox" },
   },
 }
