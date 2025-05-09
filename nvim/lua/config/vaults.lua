7f1e6cec2713c57f96e6617d5e7001199339e253
Brian Rodriguez <brian@brianrodri.com>
2025-05-09 02:18:25 -0400

    refactor: reduce dependencies

--- nvim/lua/config/vaults.lua
+++ nvim/lua/config/vaults.lua
@@ -8,7 +8,7 @@ local PRETTY_DATE_FORMAT = "%a %B %-d %Y"
 
 local INBOX_NOTE_PATH = "0 - Index/Inbox.md"
 
-local TEMPLATE_FILENAME_FORMAT = {
+local FILENAME_FORMAT_MAP = {
   ["Daily Template"] = "%Y-%m-%d",
   ["Weekly Template"] = "%Y-W%V",
   ["Monthly Template"] = "%Y-%m",
@@ -16,49 +16,44 @@ local TEMPLATE_FILENAME_FORMAT = {
 }
 
 ---@param path string
----@return obsidian.Client, obsidian.Note
----@private
+---@return obsidian.Client client, obsidian.Note note
 local function connect_with_note(path)
-  local obsidian = assert(require("obsidian"))
-  local client = assert(obsidian.get_client())
-  local note = assert(client:resolve_note(path))
-  assert(note:exists(), "Obsidian Note could not be found: " .. path)
+  local plugin = assert(require("obsidian"), "obsidian.nvim plugin is nil")
+  local client = assert(plugin.get_client(), "obsidian.nvim client is nil")
+  local note = client:resolve_note(path)
+  assert(note and note:exists(), string.format("error resolving path: %s", path))
   return client, note
 end
 
----@param target obsidian.Path
----@param template obsidian.Path
----@return integer
----@private
-local resolve_date = function(target, template)
-  return assert(utils.try_parse(TEMPLATE_FILENAME_FORMAT[template.stem], target.stem))
+---@param target? obsidian.Path
+---@param template? obsidian.Path
+---@return integer time
+local resolve_time = function(target, template)
+  assert(target and target:is_file(), string.format("invalid target path: %s", target))
+  assert(template and template:is_file(), string.format("invalid template path: %s", template))
+  local format = assert(FILENAME_FORMAT_MAP[template.stem], string.format("unknown template: %s", template))
+  return assert(utils.try_parse(format, target.stem))
 end
 
 ---@param date_format string
 ---@param delta_days? integer
 ---@return fun(time: integer): string
----@private
 local function date_formatter(date_format, delta_days)
   local delta_time = (delta_days or 0) * ONE_DAY
   return function(time) return assert(utils.try_format(date_format, time + delta_time)) end
 end
 
 ---@param date_format string
----@param date_sep? string
 ---@param delta_days? integer
+---@param date_sep? string
 ---@return fun(time: integer): string
----@private
-local function week_formatter(date_format, date_sep, delta_days)
+local function week_formatter(date_format, delta_days, date_sep)
   local format_date = date_formatter(date_format, delta_days)
   return function(time)
     local iso_week = assert(utils.try_format(ISO_WEEK_FORMAT, time))
     local start_of_week = assert(utils.try_parse(ISO_WEEK_FORMAT, iso_week))
-    return string.format(
-      "%s%s%s",
-      format_date(start_of_week),
-      date_sep or " - ",
-      format_date(start_of_week + 6 * ONE_DAY)
-    )
+    local end_of_week = start_of_week + 6 * ONE_DAY
+    return vim.fn.join({ format_date(start_of_week), format_date(end_of_week) }, date_sep or " - ")
   end
 end
 
@@ -84,7 +79,6 @@ return {
     name = "Vault",
     ---@diagnostic disable-next-line: missing-fields
     overrides = {
-      notes_subdir = "/",
       note_path_func = function(spec)
         if utils.try_parse("%Y-%m-%d", spec.id) then
           return string.format("1 - Journal/Daily/%s.md", spec.id)
@@ -94,27 +88,30 @@ return {
           return string.format("2 - Fleeting Notes/%s.md", spec.id)
         end
       end,
-      note_id_func = utils.identity,
       disable_frontmatter = true,
       use_advanced_uri = true,
 
       ---@diagnostic disable-next-line: missing-fields
-      attachments = { img_folder = "8 - Meta/Attachments" },
+      attachments = {
+        img_folder = "8 - Meta/Attachments",
+      },
       ---@diagnostic disable-next-line: missing-fields
-      daily_notes = { folder = "1 - Journal/Daily" },
+      daily_notes = {
+        folder = "1 - Journal/Daily",
+      },
       ---@diagnostic disable-next-line: missing-fields
       templates = {
         folder = "8 - Meta/Neovim Templates",
         substitutions = {
-          pretty_date = utils.compose(resolve_date, date_formatter(PRETTY_DATE_FORMAT)),
-          pretty_week = utils.compose(resolve_date, week_formatter(PRETTY_DATE_FORMAT)),
-          curr_iso_date = utils.compose(resolve_date, date_formatter(ISO_DATE_FORMAT)),
-          prev_iso_date = utils.compose(resolve_date, date_formatter(ISO_DATE_FORMAT, -1)),
-          next_iso_date = utils.compose(resolve_date, date_formatter(ISO_DATE_FORMAT, 1)),
-          curr_iso_week = utils.compose(resolve_date, date_formatter(ISO_WEEK_FORMAT)),
-          prev_iso_week = utils.compose(resolve_date, date_formatter(ISO_WEEK_FORMAT, -7)),
-          next_iso_week = utils.compose(resolve_date, date_formatter(ISO_WEEK_FORMAT, 7)),
-          curr_iso_week_number = utils.compose(resolve_date, date_formatter("%-V")),
+          pretty_date = utils.compose(resolve_time, date_formatter(PRETTY_DATE_FORMAT)),
+          pretty_week = utils.compose(resolve_time, week_formatter(PRETTY_DATE_FORMAT)),
+          curr_iso_date = utils.compose(resolve_time, date_formatter(ISO_DATE_FORMAT)),
+          prev_iso_date = utils.compose(resolve_time, date_formatter(ISO_DATE_FORMAT, -1)),
+          next_iso_date = utils.compose(resolve_time, date_formatter(ISO_DATE_FORMAT, 1)),
+          curr_iso_week = utils.compose(resolve_time, date_formatter(ISO_WEEK_FORMAT)),
+          prev_iso_week = utils.compose(resolve_time, date_formatter(ISO_WEEK_FORMAT, -7)),
+          next_iso_week = utils.compose(resolve_time, date_formatter(ISO_WEEK_FORMAT, 7)),
+          curr_iso_week_number = utils.compose(resolve_time, date_formatter("%-V")),
         },
       },
     },
