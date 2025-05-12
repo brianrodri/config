3d325cbbc4724bd0197e550609fa3da698d2a816
Brian Rodriguez <brian@brianrodri.com>
2025-05-12 14:18:28 -0400

    fix: lua linter

--- nvim/lua/config/lsp/lua-lang.lua
+++ nvim/lua/config/lsp/lua-lang.lua
@@ -4,4 +4,4 @@ vim.lsp.enable("lua_ls")
 -- Lint
 local ok, lint = pcall(require, "lint")
 if not ok then return vim.notify('require("lint") error', vim.log.levels.ERROR) end
-lint.linters_by_ft = vim.tbl_deep_extend("force", lint.linters_by_ft or {}, { markdown = { "luacheck" } })
+lint.linters_by_ft = vim.tbl_deep_extend("force", lint.linters_by_ft or {}, { lua = { "luacheck" } })
