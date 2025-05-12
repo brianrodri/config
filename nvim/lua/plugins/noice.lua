6645eabb1a46fd3cc5a535a084d3c760bcf46581
Brian Rodriguez <brian@brianrodri.com>
2025-05-12 13:11:48 -0400

    feat: enable noice presets

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -14,6 +14,13 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
         },
       },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
