f47664649cc35abc90aa1f5f8f2d636e3d530372
Brian Rodriguez <brian@brianrodri.com>
2025-05-13 07:38:50 -0400

    refactor: use default opts rather than always passing them around

--- nvim/lua/plugins/core/formatting.lua
+++ nvim/lua/plugins/core/formatting.lua
@@ -1,7 +1,3 @@
-local utils = require("my.utils")
-
-local FORMAT_OPTS = { lsp_format = "fallback" }
-
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -10,8 +6,11 @@ return {
   cmd = { "ConformInfo" },
   ---@module "conform"
   ---@type conform.setupOpts
-  opts = { format_on_save = function() return utils.get_var("autoformat") and FORMAT_OPTS or nil end },
+  opts = {
+    default_format_opts = { lsp_format = "fallback" },
+    format_on_save = function() return require("my.utils").get_var("autoformat") and {} or nil end,
+  },
   keys = {
-    { "<leader>cq", function() require("conform").format(FORMAT_OPTS) end, desc = "Format" },
+    { "<leader>cq", function() require("conform").format() end, desc = "Format" },
   },
 }
