4e41d5f21a370eae23fbb572cc33564368a76bcd
Brian Rodriguez <brian@brianrodri.com>
2025-05-13 08:29:47 -0400

    feat: add gdscript support

---
+++ nvim/lua/plugins/lang/godot.lua
@@ -0,0 +1,26 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "gdscript" } },
+    opts_extend = { "ensure_installed" },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = { formatters_by_ft = { gdscript = { "gdformat" } } },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@type my.LspConfig
+    opts = { gdscript = {} },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    ---@type my.LintOpts
+    opts = { linters_by_ft = { gdscript = { "gdlint" } } },
+  },
+}
