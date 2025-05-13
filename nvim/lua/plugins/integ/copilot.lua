8774a7396a672bf8bf67fb64ee7498c350cac30a
Brian Rodriguez <brian@brianrodri.com>
2025-05-13 10:12:12 -0400

    feat: install copilot

---
+++ nvim/lua/plugins/integ/copilot.lua
@@ -0,0 +1,33 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    ---@module "copilot"
+    ---@type CopilotConfig
+    opts = {},
+  },
+
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "giuxtaposition/blink-cmp-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
