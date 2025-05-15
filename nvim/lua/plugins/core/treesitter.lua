da2da3c4b60db9767f09a12939b5808af3fdf043
Brian Rodriguez <brian@brianrodri.com>
2025-05-15 10:56:16 -0400

    feat: install treesitter-textobjects

--- nvim/lua/plugins/core/treesitter.lua
+++ nvim/lua/plugins/core/treesitter.lua
@@ -1,9 +1,23 @@
+local utils = require("my.utils")
+
 ---@module "lazy"
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   main = "nvim-treesitter.configs",
-  opts = { highlight = { enable = true } },
+  opts = {
+    indent = { enable = true },
+    highlight = { enable = true },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    },
+  },
   config = function(_, opts)
     opts.ensure_installed = utils.dedupe(opts.ensure_installed)
     require("nvim-treesitter.configs").setup(opts)
