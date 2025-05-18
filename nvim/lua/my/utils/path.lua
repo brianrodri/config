ec4f16994f60506cdbaba2856649de5fae0edfd7
Brian Rodriguez <brian@brianrodri.com>
2025-05-18 09:49:22 -0400

    feat: Path:name(), Path:stem(), Path:exists()

--- nvim/lua/my/utils/path.lua
+++ nvim/lua/my/utils/path.lua
@@ -3,7 +3,6 @@ local Fmts = require("my.utils.fmts")
 
 ---@class my.Path
 ---@field path string
----@field resolved boolean
 local Path = {
   __tostring = function(self) return self.path end,
   __eq = function(self, other) return self.path == other.path end,
@@ -26,7 +25,7 @@ function Path.join(...)
   local ok, result = pcall(vim.fs.joinpath, unpack(vim.tbl_map(tostring, path_parts)))
   assert(ok, Fmts.call_error(result, "Path.join", ...))
   local self = setmetatable({}, Path)
-  self.path, self.resolved = result, false
+  self.path = result
   return self
 end
 
@@ -59,6 +58,41 @@ function Path.stdpath(what)
   return type(result) == "table" and vim.iter(result):map(Path.join):totable() or Path.join(result)
 end
 
+--- Wrapper around |vim.fs.basename()|.
+---
+---@return string|? basename
+function Path:basename() return vim.fs.basename(self.path) end
+
+--- Returns the |vim.fs.basename()| without the extension (i.e. the text after and including the final ".").
+---
+---@return string|? stem
+function Path:stem()
+  local basename = self:basename()
+  if not basename then return nil end
+  local parts = vim.iter(vim.split(basename, "."))
+  parts:pop()
+  return parts:join(".")
+end
+
+--- Wrapper around |vim.fs.dirname()|.
+---
+---@return my.Path|? dirname
+function Path:dirname()
+  local dirname = vim.fs.dirname(self.path)
+  return dirname and Path.join(dirname)
+end
+
+--- Wrapper around |vim.fs.normalize()|.
+---
+---@param opts? vim.fs.normalize.Opts
+---@return my.Path normalized_path
+function Path:normalize(opts) return Path.join(vim.fs.normalize(self.path, opts)) end
+
+--- Wrapper around |uv.fs_stat()|.
+---
+---@return boolean exists
+function Path:exists() return vim.uv.fs_stat(self.path) ~= nil end
+
 --- Wrapper around |io.open()| to ensure that |file:close()| is always called.
 ---
 ---@param mode openmode
@@ -83,16 +117,8 @@ function Path:make_directory() return vim.fn.mkdir(self.path, "p") == 1 end
 function Path:resolve()
   local realpath, err = vim.uv.fs_realpath(self.path)
   assert(realpath, Fmts.call_error(err, "Path.resolve", self))
-  self.path, self.resolved = realpath, true
+  self.path = realpath
   return self
-end
-
---- Wrapper around |fs_stat()|.
----
----@return uv.fs_stat.result
-function Path:status()
-  local stat, err = vim.uv.fs_stat(self.path)
-  return assert(stat, Fmts.call_error(err, "Path.status", self))
 end
 
 --- Wrapper around |isdirectory()|.
