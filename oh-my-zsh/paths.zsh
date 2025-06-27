71844b045a01781c661889cd9a9cb6057fd0db48
Brian Rodriguez <brian@brianrodri.com>
2025-06-27 18:56:18 -0400

    chore: check for mise installations so that lua@5.1 is used

--- oh-my-zsh/paths.zsh
+++ oh-my-zsh/paths.zsh
@@ -7,6 +7,10 @@ if [ -f "$(command -v brew)" ]; then
   export PATH="$(brew --prefix)/opt/llvm/bin:$PATH"
 fi
 
+if [ -f "$(command -v mise)" ]; then
+  export PATH="$HOME/.local/share/mise/installs/lua/5.1/bin:$PATH"
+fi
+
 if [ -f "$(command -v gem)" ]; then
   # Ruby is not linked by default to avoid conflicts with macOS's builtins.
   export PATH="$(brew --prefix)/opt/ruby/bin:$PATH"
