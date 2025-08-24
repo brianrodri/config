8adac045a3914030679689620db1f320dd81c5d9
Brian Rodriguez <brian@brianrodri.com>
2025-08-24 14:22:00 -0400

    fix: stop depending on pyenv

--- oh-my-zsh/sources.zsh
+++ oh-my-zsh/sources.zsh
@@ -7,10 +7,3 @@ if [ -d "$HOME/.perl5" ]; then
   # https://metacpan.org/pod/local::lib#The-bootstrapping-technique
   eval "$(perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)"
 fi
-
-if [ -f "$(command -v pyenv)" ]; then
-  # https://github.com/pyenv/pyenv#b-set-up-your-shell-environment-for-pyenv
-  eval "$(pyenv init --path)"
-  # https://github.com/pyenv/pyenv-virtualenv#installing-with-homebrew-for-macos-users
-  eval "$(pyenv virtualenv-init -)"
-fi
