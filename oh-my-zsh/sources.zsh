83059fce516388bbb6bf8491e25af2664ddaf274
Brian Rodriguez <brian@brianrodri.com>
2026-02-10 19:16:15 -0500

    feat(cargo): avoid depending on homebrew installation

--- oh-my-zsh/sources.zsh
+++ oh-my-zsh/sources.zsh
@@ -3,6 +3,10 @@ if [ -f "$(command -v oh-my-posh)" ]; then
   eval "$(oh-my-posh init zsh --config "$HOME/.oh-my-posh.yaml")"
 fi
 
+if [ -f "$(command -v cargo)" ]; then
+  source "$HOME/.cargo/env"
+fi
+
 if [ -d "$HOME/.perl5" ]; then
   # https://metacpan.org/pod/local::lib#The-bootstrapping-technique
   eval "$(perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)"
